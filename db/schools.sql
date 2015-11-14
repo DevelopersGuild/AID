e84ffa48c038eade43293c448f2ce1ca1be1384e
Wolfgang C. Strack <wolfgangcstrack@gmail.com>
2015-11-13 16:52:34 -0800

    Add schools table sql

---
+++ db/schools.sql
@@ -0,0 +1,9 @@
+-- schools table
+
+CREATE TABLE Schools(
+  id INT(10) PRIMARY KEY,
+  time_created TIMESTAMP NOT NULL,
+  last_updated TIMESTAMP NOT NULL,
+  assist_code VARCHAR(10) NOT NULL,
+  full_name VARCHAR(50)
+);
